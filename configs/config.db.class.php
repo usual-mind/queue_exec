1389bc7f1868c72a582ed4631968b3e6720edb77
孙传亮 <754442114@qq.com>
2017-07-13 13:22:43 +0800

    数据库配置

---
+++ configs/config.db.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sunchuanliang
+ * Date: 2017/7/11
+ * Time: 12:49
+ */
+$db_config = array(
+    'hostname' => '115.159.103.226',
+    'username' => 'sun',
+    'password' => '123456',
+    'database' => 'Imooc',
+    'charset' => 'utf8',
+    'pconnect' => 0,                    //是否使用持久连接(0关/1开)
+    'log' => 1,                            //是否开启日志(0关/1开)
+    'logfilepath' => '../log/',                //日志文件目录
+);
